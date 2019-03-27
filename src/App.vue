e06636f0f4d9a8ee9a6e5d52533ca22292740fd4
Yuichi Yogo <yuichkun@gmail.com>
2019-03-27 19:57:59 +0900

    update close script

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,11 @@ const socket = io(`http://localhost:${socketServerPort}`);
 socket.on("connect", () => {
   console.log(`connected with your M4L! at ${socketServerPort}`);
 });
+socket.on("broadcast", msg=>{
+  if(msg === 'CLOSE'){
+    window.close();
+  }
+});
 socket.on("disconnect", ()=>{
   window.close();
 });
