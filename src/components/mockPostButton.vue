17d37151f85286dba25b3719ed9746e69ba253da
szk2s <suzukisjss@gmail.com>
2019-03-31 22:10:17 +0900

    Implement feature formatting partials into PWT

--- src/components/mockPostButton.vue
+++ src/components/mockPostButton.vue
@@ -3,22 +3,27 @@
 </template>
 
 <script>
+import formatAsPwt from '../modules/formatAsPwt';
+
 export default {
   methods: {
     async postImage(){
       console.log('post');
       const img = await fetch('./mockSpectrogram.png')
       const ab = await img.arrayBuffer();
-      const res = await fetch('http://54.238.234.108:5000', {
+      const res = await fetch('http://127.0.0.1:5000', {
         method: 'POST',
         body: ab,
         mode: 'cors'
       })
       .then(d=> d.json())
       .catch(e => {
+        console.error(e)
         debugger;
       });
-      console.log(res);
+      
+      const pwt = formatAsPwt(ab, res);
+      debugger;
     }
   }
 }
