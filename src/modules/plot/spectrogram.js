d960babd8af9281c0efb7dedc7a71279fc28c6f3
szk2s <suzukisjss@gmail.com>
2019-04-08 01:25:52 +0900

    Refine spectrogram.js
    - Optimize fft
    - Refactor code requesting animation frame
    - Refine WindowSize management
    - Add getFreqs function

--- src/modules/plot/spectrogram.js
+++ src/modules/plot/spectrogram.js
@@ -1,19 +1,22 @@
-import { DFT, WindowFunction } from 'dsp.js-browser';
+import { FFT, WindowFunction } from 'dsp.js-browser';
 import gainToDecibels from 'decibels/from-gain';
 
 // subfunction to plot spectrogram
 const spectrogram = (audioBuffer, canvas, _windowSize, sr) => {
-  debugger;
   const win = {};
-  win.size = Math.round( _windowSize / 4) * 4;
   const originalFloatArray = audioBuffer.getChannelData(0);
+  // window size must be a power of 2
+  if ( _windowSize > originalFloatArray.length) {
+    win.size = 2 ** Math.floor( Math.log2( originalFloatArray.length ) )
+  } else {
+    win.size = 2 ** Math.round( Math.log2( _windowSize ) )
+  }
   const stepSize = win.size / 4 ;
-  win.getLeftEdgeSampleIdx = i => i*stepSize;
-  win.getRightEdgeSampleIdx = i => (win.size-1) + i*stepSize;
-  win.getCenterSampleIdx = i => win.size/2 + i*stepSize;
+  win.getLeftEdgeSampleIdx = windowIdx => windowIdx*stepSize;
+  win.getRightEdgeSampleIdx = windowIdx => (win.size-1) + windowIdx*stepSize;
+  win.getCenterSampleIdx = windowIdx => win.size/2 + windowIdx*stepSize;
   const windowFunction = new WindowFunction(7); // "7" corresponds to HANN window 
-  const dft = new DFT(win.size, sr);
-  debugger;
+  const fft = new FFT(win.size, sr);
   // Set up Canvas
   const ctx = canvas.getContext('2d');
   // ctx.globalCompositeOperation = 'screen';
@@ -26,43 +29,50 @@ const spectrogram = (audioBuffer, canvas, _windowSize, sr) => {
   let i = 0; 
 
   const plotColumn = () => {
-    if (win.getRightEdgeSampleIdx(i) < originalFloatArray.length) {
-      // Run windowing and DFT
-      const slicedBuffer = originalFloatArray.slice(win.getLeftEdgeSampleIdx(i), win.getRightEdgeSampleIdx(i) + 1);
-      const windowedBuffer = windowFunction.process(slicedBuffer);
-      dft.forward(windowedBuffer);
-      // @ts-ignore
-      win.spectrum = dft.spectrum;
-      
-      // Plot rectangles
-      const numberOfSamples = originalFloatArray.length;
-      const numberOfRows = win.spectrum.length;
-      const rect = {}
-      rect.center = {}
-      rect.center.x = canvas.width * win.getCenterSampleIdx(i) / numberOfSamples;
-      rect.width = canvas.width * win.size / numberOfSamples;
-      rect.height = canvas.height / numberOfRows;
-      win.spectrum.forEach((amp, rowIdx) => {
-        rect.center.y = canvas.height * (numberOfRows - 1 - rowIdx) /numberOfRows;
-        const blackThreshold = -78 // in dB
-        const luminance = ( Math.max(gainToDecibels(amp) , blackThreshold) + Math.abs(blackThreshold) ) / Math.abs(blackThreshold);
-        ctx.fillStyle = 
-          'rgb(' + 
-          SPECTROGRAM_COLOR.r * luminance + ', ' +  
-          SPECTROGRAM_COLOR.g * luminance + ', ' +  
-          SPECTROGRAM_COLOR.b * luminance + ')';
-        ctx.fillRect(
-          rect.center.x - rect.width / 2, 
-          rect.center.y - rect.height / 2, 
-          rect.width,
-          rect.height
-        );
-      })
-    }; 
+    // Run windowing and FFT
+    const slicedBuffer = originalFloatArray.slice(win.getLeftEdgeSampleIdx(i), win.getRightEdgeSampleIdx(i) + 1);
+    const windowedBuffer = windowFunction.process(slicedBuffer);
+    fft.forward(windowedBuffer);
+    // @ts-ignore
+    win.spectrum = fft.spectrum;
+    
+    // Plot rectangles
+    const numberOfSamples = originalFloatArray.length;
+    const numberOfRows = win.spectrum.length;
+    const rect = {}
+    rect.center = {}
+    rect.center.x = canvas.width * win.getCenterSampleIdx(i) / numberOfSamples;
+    rect.width = canvas.width * win.size / numberOfSamples;
+    rect.height = canvas.height / numberOfRows;
+    win.spectrum.forEach((amp, rowIdx) => {
+      rect.center.y = canvas.height * (numberOfRows - 1 - rowIdx) /numberOfRows;
+      const blackThreshold = -78 // in dB
+      const luminance = ( Math.max(gainToDecibels(amp) , blackThreshold) + Math.abs(blackThreshold) ) / Math.abs(blackThreshold);
+      ctx.fillStyle = 
+        'rgb(' + 
+        SPECTROGRAM_COLOR.r * luminance + ', ' +  
+        SPECTROGRAM_COLOR.g * luminance + ', ' +  
+        SPECTROGRAM_COLOR.b * luminance + ')';
+      ctx.fillRect(
+        rect.center.x - rect.width / 2, 
+        rect.center.y - rect.height / 2, 
+        rect.width,
+        rect.height
+      );
+    })
     i++;
-    window.requestAnimationFrame(plotColumn);
+    if (win.getRightEdgeSampleIdx(i) < originalFloatArray.length) {
+      window.requestAnimationFrame(plotColumn);
+    }
   }
-  
+
+  const getFreqs = (windowSize, fft) => {
+    const numberOfFrequencyBins = windowSize / 2;
+    const freqs = Array.from({ length: numberOfFrequencyBins }, (_, i) => i).map(i => fft.getBandFrequency(i));
+    return freqs;
+  }
+
+  console.log(getFreqs(win.size, fft));
   window.requestAnimationFrame(plotColumn);
 }
 export default spectrogram;
