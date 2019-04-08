f88f87ebb1078bbb60957062a546016b8ea69c1e
szk2s <suzukisjss@gmail.com>
2019-04-08 16:10:24 +0900

    Refine spectrogram module
    - Change color
    - Change the declaration position of getFreqs
    - Add JSDoc

--- src/modules/plot/spectrogram.js
+++ src/modules/plot/spectrogram.js
@@ -1,11 +1,17 @@
 import { FFT, WindowFunction } from 'dsp.js-browser';
 import gainToDecibels from 'decibels/from-gain';
 
-// subfunction to plot spectrogram
+/**
+ * Calculate FFT on audio buffer and plot a spectrogram on canvas element.
+ * @param  {AudioBuffer} audioBuffer
+ * @param  {HTMLCanvasElement} canvas
+ * @param  {number} _windowSize In number of samples. This will be replaced with the nearest power of 2
+ * @param  {number} sr Sampling rate of audio buffer. Integer
+ */
 const spectrogram = (audioBuffer, canvas, _windowSize, sr) => {
   const win = {};
   const originalFloatArray = audioBuffer.getChannelData(0);
-  // window size must be a power of 2
+  // window size must be power of 2
   if ( _windowSize > originalFloatArray.length) {
     win.size = 2 ** Math.floor( Math.log2( originalFloatArray.length ) )
   } else {
@@ -17,13 +23,18 @@ const spectrogram = (audioBuffer, canvas, _windowSize, sr) => {
   win.getCenterSampleIdx = windowIdx => win.size/2 + windowIdx*stepSize;
   const windowFunction = new WindowFunction(7); // "7" corresponds to HANN window 
   const fft = new FFT(win.size, sr);
+  const getFreqs = (windowSize, fft) => {
+    const numberOfFrequencyBins = windowSize / 2;
+    const freqs = Array.from({ length: numberOfFrequencyBins }, (_, i) => i).map(i => fft.getBandFrequency(i));
+    return freqs;
+  }
   // Set up Canvas
   const ctx = canvas.getContext('2d');
   // ctx.globalCompositeOperation = 'screen';
   const SPECTROGRAM_COLOR = {
-    r: 225,
+    r: 255,
     g: 255,
-    b: 50
+    b: 0
   }
 
   let i = 0; 
@@ -64,12 +75,6 @@ const spectrogram = (audioBuffer, canvas, _windowSize, sr) => {
     if (win.getRightEdgeSampleIdx(i) < originalFloatArray.length) {
       window.requestAnimationFrame(plotColumn);
     }
-  }
-
-  const getFreqs = (windowSize, fft) => {
-    const numberOfFrequencyBins = windowSize / 2;
-    const freqs = Array.from({ length: numberOfFrequencyBins }, (_, i) => i).map(i => fft.getBandFrequency(i));
-    return freqs;
   }
 
   console.log(getFreqs(win.size, fft));
