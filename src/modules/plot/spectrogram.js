7fb133c75421201bc3fcf566fe8f7a9201827473
szk2s <suzukisjss@gmail.com>
2019-04-09 14:30:14 +0900

    Complete implementation of spectrogram module
    - Make it return the result of STFT
    - Refactor for improving readability

--- src/modules/plot/spectrogram.js
+++ src/modules/plot/spectrogram.js
@@ -1,19 +1,24 @@
 import { FFT, WindowFunction } from 'dsp.js-browser';
 import gainToDecibels from 'decibels/from-gain';
 import { ftom } from '../utils';
+import packIntoNdarray from 'ndarray-pack';
+import unpackFromNdArray from 'ndarray-unpack'
 
 /**
- * Calculate FFT on audio buffer and plot a spectrogram on canvas element.
+ * Calculate STFT, Short Time Fourier Transform, on audio buffer and plot a spectrogram on canvas element.
  * @param  {AudioBuffer} audioBuffer
  * @param  {HTMLCanvasElement} canvas
  * @param  {number} _windowSize In number of samples. This will be replaced with the nearest power of 2
  * @param  {number} sr Sampling rate of audio buffer. Integer
- * @return {{freqs: Array.<number>, amp2d: Array.<Array<number>>}} freqs is frequency in Hz from low to high.
+ * @return {Promise.<{times: Array.<number>, freqs: Array.<number>, magnitude2d: Array.<Array<number>>}>} freqs is decimal values in Hz. From low to high. magnitude2d is decimals from 0. to 1. From low freq to high freq. 
  */
-const spectrogram = (audioBuffer, canvas, _windowSize, sr) => {
+const spectrogram = (audioBuffer, canvas, _windowSize, sr) => new Promise(resolve => {
   const win = {};
   const originalFloatArray = audioBuffer.getChannelData(0);
+  
+  // NEED REFACTOR
   // window size must be power of 2
+  // minimum value is 8
   if ( _windowSize > originalFloatArray.length) {
     win.size = 2 ** Math.floor( Math.log2( originalFloatArray.length ) )
   } else {
@@ -39,11 +44,10 @@ const spectrogram = (audioBuffer, canvas, _windowSize, sr) => {
   }
   // Set up Canvas
   const ctx = canvas.getContext('2d');
-  // ctx.globalCompositeOperation = 'screen';
   const SPECTROGRAM_COLOR = {
-    r: 255,
-    g: 255,
-    b: 0
+    R: 255,
+    G: 255,
+    B: 0
   }
 
   // calculate Y position of each row
@@ -52,43 +56,56 @@ const spectrogram = (audioBuffer, canvas, _windowSize, sr) => {
   const highestNote = midiNoteNums[midiNoteNums.length - 1];
   const lowestNote = midiNoteNums[0];
   const rowPositions = midiNoteNums.map(noteNum => (highestNote - noteNum) / (highestNote - lowestNote));
-  
-  const result = {freqs, amp2d: [[]]}
-  let i = 0; 
 
+  const resultOfSTFT = {
+    times: [], 
+    freqs: freqs, 
+    magnitude2d: null
+  }
+  let i = 0; 
+  /**@type {Array.<Array>} Array of spectrum (magnitude values from low freq bin to high freq bin)  */
+  const spectra = [];
   const plotColumn = () => {
     // Run windowing and FFT
     const slicedBuffer = originalFloatArray.slice(win.getLeftEdgeSampleIdx(i), win.getRightEdgeSampleIdx(i) + 1);
+    resultOfSTFT.times.push(win.getCenterSampleIdx(i) / sr);
     const windowedBuffer = windowFunction.process(slicedBuffer);
     fft.forward(windowedBuffer);
     // @ts-ignore
     win.spectrum = fft.spectrum;
+    spectra.push(Array.from(win.spectrum));
+    // spectra.push(fft.spectrum)
     
     // Plot rectangles
+    /** @type {number} How many samples is in the original buffer  */
     const numberOfSamples = originalFloatArray.length;
     const numberOfRows = win.spectrum.length;
-    win.spectrum.forEach((amp, rowIdx) => {
-      const rect = {}
-      rect.center = {}
-      rect.center.x = canvas.width * win.getCenterSampleIdx(i) / numberOfSamples;
-      rect.center.y = canvas.height * rowPositions[rowIdx];
-      rect.isLowestRect = rowIdx === 0;
-      rect.isUpmostRect = rowIdx === numberOfRows.length - 1
+    win.spectrum.forEach((magnitude, rowIdx) => {
+      const blackThreshold = -78 // in dB
+      const rect = {
+        center: {
+          x: canvas.width * win.getCenterSampleIdx(i) / numberOfSamples,
+          y: canvas.height * rowPositions[rowIdx]
+        },
+        isLowestRect: rowIdx === 0,
+        isUpmostRect: rowIdx === numberOfRows.length - 1,
+        luminance: ( Math.max(gainToDecibels(magnitude) , blackThreshold) + Math.abs(blackThreshold) ) / Math.abs(blackThreshold),
+      }
       const oneLowerRectCenterY = rect.isLowestRect ? canvas.height : canvas.height * rowPositions[rowIdx - 1];
       const oneUpperRectCenterY = rect.isUpmostRect ? 0 : canvas.height * rowPositions[rowIdx + 1];
-      rect.lowerEdge = {};
-      rect.upperEdge = {};
-      rect.lowerEdge.y = (oneLowerRectCenterY + rect.center.y) / 2;
-      rect.upperEdge.y = (oneUpperRectCenterY + rect.center.y) / 2;
+      rect.lowerEdge = {
+        y: (oneLowerRectCenterY + rect.center.y) / 2
+      };
+      rect.upperEdge = {
+        y: (oneUpperRectCenterY + rect.center.y) / 2
+      };
       rect.width = canvas.width * win.size / numberOfSamples;
       rect.height = rect.lowerEdge.y - rect.upperEdge.y;
-      const blackThreshold = -78 // in dB
-      const luminance = ( Math.max(gainToDecibels(amp) , blackThreshold) + Math.abs(blackThreshold) ) / Math.abs(blackThreshold);
       ctx.fillStyle = 
         'rgb(' + 
-        SPECTROGRAM_COLOR.r * luminance + ', ' +  
-        SPECTROGRAM_COLOR.g * luminance + ', ' +  
-        SPECTROGRAM_COLOR.b * luminance + ')';
+        SPECTROGRAM_COLOR.R * rect.luminance + ', ' +  
+        SPECTROGRAM_COLOR.G * rect.luminance + ', ' +  
+        SPECTROGRAM_COLOR.B * rect.luminance + ')';
       ctx.fillRect(
         rect.center.x - rect.width / 2, 
         rect.upperEdge.y, 
@@ -99,10 +116,12 @@ const spectrogram = (audioBuffer, canvas, _windowSize, sr) => {
     i++;
     if (win.getRightEdgeSampleIdx(i) < originalFloatArray.length) {
       window.requestAnimationFrame(plotColumn);
+    } else {
+      resultOfSTFT.magnitude2d = unpackFromNdArray(packIntoNdarray(spectra).transpose(1, 0));  
+      resolve(resultOfSTFT);
     }
   }
-  window.requestAnimationFrame(plotColumn);
 
-  return result;
-}
+  window.requestAnimationFrame(plotColumn);
+})
 export default spectrogram;
