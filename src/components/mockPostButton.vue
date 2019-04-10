455723036631ccf3e0bc77db97fd4b6b26ddf80d
Yuichi Yogo <yuichkun@gmail.com>
2019-04-10 14:10:15 +0900

    tmp

--- src/components/mockPostButton.vue
+++ src/components/mockPostButton.vue
@@ -1,29 +1,36 @@
 <template>
-  <button @click="postImage">post</button>
+  <div>
+    <!-- <h2>the result of stft: {{ stftData }}</h2> -->
+    <button @click="postImage">post</button>
+  </div>
 </template>
 
 <script>
-import formatAsPwt from '../modules/formatAsPwt';
-
+import { encode } from 'fast-png'
 export default {
+  props: {
+    resultOfSTFT: {times: Array, freqs: Array, magnitude2d: Array }
+  },
   methods: {
     async postImage(){
       console.log('post');
-      const img = await fetch('./mockSpectrogram.png')
-      const ab = await img.arrayBuffer();
+      const img = encode({
+        width: this.resultOfSTFT.magnitude2d[0].length,
+        height: this.resultOfSTFT.magnitude2d.length,
+        data: this.resultOfSTFT.magnitude2d.flat(),
+        depth: 8,
+        channels: 1
+      })
       const res = await fetch('https://54.238.234.108:5000', {
         method: 'POST',
-        body: ab,
+        body: img.buffer,
         mode: 'cors'
       })
       .then(d=> d.json())
       .catch(e => {
-        console.error(e)
         debugger;
       });
-      
-      const pwt = formatAsPwt(ab, res);
-      debugger;
+      console.log(res);
     }
   }
 }
