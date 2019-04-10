ab3fc7ecc409d2d661ad52948430255a484ff3ee
szk2s <suzukisjss@gmail.com>
2019-04-10 19:37:10 +0900

    Refactor component structure
    Separate playAudioBuffer function
    Separate Viewer somponent

---
+++ src/components/Viewer.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class=viewer>
+      <canvas ref="waveform" width="800" height="300">waveform</canvas>
+      <canvas ref="spectrogram" width="800" height="300">spectrogram</canvas>
+    </div>
+</template>
+
+<script>
+import { resample } from '../modules/audioBufferProcess'
+import { waveform, spectrogram } from '../modules/plot'
+
+export default {
+  name: 'Viewer',
+  props: {
+    sourceAudioBuffer: AudioBuffer
+  },
+  created() {
+    this.$eventHub.$on('audio-is-ready', this.plot);
+  },
+  methods: {
+    plot: async function (audioBuffer) {
+      console.log(audioBuffer.length);
+      const DESIRED_SAMPLE_RATE = 22050;
+      const windowSize = 1987;
+      const resampleEvent = await resample(audioBuffer, DESIRED_SAMPLE_RATE);
+      const resampledAudioBuffer = resampleEvent.renderedBuffer;
+      waveform(resampledAudioBuffer, this.$refs.waveform);
+      const resultOfSTFT = await spectrogram(resampledAudioBuffer, this.$refs.spectrogram, windowSize, DESIRED_SAMPLE_RATE);
+      this.$emit('stft-completed', resultOfSTFT);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  canvas {
+    background-color: #13161A;
+  }
+</style>
---
+++ src/modules/utils/playAudioBuffer.js
@@ -0,0 +1,15 @@
+/**
+ * @param  {AudioBuffer} audioBuffer
+ */
+const playAudioBuffer = (audioBuffer) => {
+  if (audioBuffer == null) {
+    return;
+  }
+  const context = new AudioContext({latencyHint: 'interactive', sampleRate: 22050});
+  const source = context.createBufferSource(); // creates a sound source
+  source.buffer = audioBuffer;                    // tell the source which sound to play
+  source.connect(context.destination);       // connect the source to the context's destination (the speakers)
+  source.start(0);
+}
+
+export default playAudioBuffer;
