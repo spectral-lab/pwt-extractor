6417e7cec62264ed35dca9455528ff4f2958a1b6
Yuichi Yogo <yuichkun@gmail.com>
2019-04-21 18:40:32 -0400

    example: sending extra params to server

--- src/components/MockPostButton.vue
+++ src/components/MockPostButton.vue
@@ -14,9 +14,13 @@ export default {
     postImage(){
       const { spectrogram } = this.$store.state;
       const buff = makePNGBuffer(spectrogram.magnitude2d);
+      const blob = new Blob([buff], {type: "images/png"});
+      const formData = new FormData();
+      formData.append("pngImage", blob);
+      formData.append('line_continuity', 2);
       fetch(process.env.VUE_APP_SERVER, {
         method: 'POST',
-        body: buff,
+        body: formData,
         mode: 'cors'
       })
       .then(d => d.json())
