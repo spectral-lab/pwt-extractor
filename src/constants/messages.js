a893a77065da1e6858fdf78fc2d244c0c8246174
szk2s <suzukisjss@gmail.com>
2019-04-27 01:33:55 +0900

    Minor change
    - Modify comment a bit

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,6 +1,6 @@
 export const MESSAGES = {
   LOADING: "Now, building your spectrogram. Just a moment.",
-  SPECTROGRAM_BUILT: "Here is the Spectrogram. Check and click next.",
+  SPECTROGRAM_BUILT: "Here is the spectrogram. Check it and click next.",
   COMPLETE: "You have done! Now you can play Starling on Ableton Live",
 }
 
